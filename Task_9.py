5b4b4e2fbdc2da4aab3d5be1490bf08fb9355e70
Jahdie <jahdie@yandex.ru>
2021-01-22 13:08:29 +0700

    Task #9 Done

--- Task_9.py
+++ Task_9.py
@@ -1,5 +1,5 @@
 # Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
-# https://drive.google.com/file/d/1zeBMT4P-myqya7KnZUv0QJwA8cv9y8SG/view?usp=sharing
+# https://drive.google.com/file/d/1zjCdF5ZTABUqmMwkeLk06Lg9t-LYs9AC/view?usp=sharing
 
 num_1 = int(input('Введите первое число: '))
 num_2 = int(input('Введите второе число: '))
