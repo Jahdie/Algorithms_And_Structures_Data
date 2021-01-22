9e69d4559dff71ac0837a1aee910b982a7728f68
Jahdie <jahdie@yandex.ru>
2021-01-22 13:07:14 +0700

    Task #5 Done

--- Task_5.py
+++ Task_5.py
@@ -1,5 +1,5 @@
 # Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
-# https://drive.google.com/file/d/1E6DgRai4mO7GGd8oMYaQ77ZRLkKuk1-s/view?usp=sharing
+# https://drive.google.com/file/d/1zjCdF5ZTABUqmMwkeLk06Lg9t-LYs9AC/view?usp=sharing
 
 letter_1 = ord(input("Введите первую строчную букву латинского алфавита: ").lower())
 letter_2 = ord(input("Введите вторую строчную букву латинского алфавита: ").lower())
@@ -8,6 +8,6 @@ print(f"Первая буква находится на {letter_1 - 96} мест
 print(f"Вторая буква находится на {letter_2 - 96} месте в алфавите.")
 
 if letter_1 - letter_2 < 0:
-    print(f"Между ними {abs((letter_1+1) - letter_2)} букв.")
+    print(f"Между ними {abs((letter_1 + 1) - letter_2)} букв.")
 else:
-    print(f"Между ними {(letter_1-1) - letter_2} букв.")
+    print(f"Между ними {(letter_1 - 1) - letter_2} букв.")
