c4d229b66707d31026aa3cb9e67ff5e3a27f5e9b
Jahdie <jahdie@yandex.ru>
2021-01-22 13:07:32 +0700

    Task #6 Done

--- Task_6.py
+++ Task_6.py
@@ -1,9 +1,9 @@
 # Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
-# https://drive.google.com/file/d/1LjZCm0OZrYZ_R9ZKnKSd9poJoFqFLzAi/view?usp=sharing
+# https://drive.google.com/file/d/1zjCdF5ZTABUqmMwkeLk06Lg9t-LYs9AC/view?usp=sharing
 
 num = int(input("Введите номер от 1 - 26: "))
 
 if 1 <= num <= 26:
-    print(f"Буква: {chr(96+num)}")
+    print(f"Буква: {chr(96 + num)}")
 else:
     print("Число должно быть от 1 до 26.")
