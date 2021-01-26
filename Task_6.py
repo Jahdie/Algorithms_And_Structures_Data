96d2d249582858f085995f969e3d8bbe3f38430a
Jahdie <63294868+Jahdie@users.noreply.github.com>
2021-01-26 22:06:11 +0700

    Delete Task_6.py

--- Task_6.py
+++
@@ -1,9 +0,0 @@
-# Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
-# https://drive.google.com/file/d/1zjCdF5ZTABUqmMwkeLk06Lg9t-LYs9AC/view?usp=sharing
-
-num = int(input("Введите номер от 1 - 26: "))
-
-if 1 <= num <= 26:
-    print(f"Буква: {chr(96 + num)}")
-else:
-    print("Число должно быть от 1 до 26.")
