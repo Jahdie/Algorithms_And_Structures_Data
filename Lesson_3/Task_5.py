5dfb1299187e5026606c6fadaa7fc1140793720c
Jahdie <jahdie@yandex.ru>
2021-01-28 16:04:19 +0700

    Task #5

---
+++ Lesson_3/Task_5.py
@@ -0,0 +1,23 @@
+# В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+# Примечание к задаче: пожалуйста не путайте «минимальный» и «максимальный отрицательный». Это два абсолютно разных
+# значения.
+from random import randint
+
+SIZE = 10
+MIN_ITEM = -100
+MAX_ITEM = 100
+
+array = [randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+max_negative_num = []
+negative_array = []
+
+for list_index_value in enumerate(array):
+    if list_index_value[1] < 0:
+        negative_array.append(list_index_value)
+        max_negative_num = negative_array[0]
+    for i in range(len(negative_array)):
+        if negative_array[i][1] > max_negative_num[1]:
+            max_negative_num = negative_array[i]
+
+print(f"Максимальное отрицательное число {max_negative_num[1]} и имеет {max_negative_num[0]} индекс.")
